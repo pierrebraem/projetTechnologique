ba51575da8aba179d99e598bf997abd648ad911f
Pierre Braem <pierrebraem@orange.fr>
2022-05-06 16:32:14 -0400

    Transfère du dataRead dans le stone.cpp

--- Stone/InterfaceEnfichable/main.cpp
+++ Stone/InterfaceEnfichable/main.cpp
@@ -61,16 +61,6 @@ int NbrePlugIns = 0;
 int NbreAddon = 0;
 static Stone *stone;
 
-// Structure local utilisés pour garder les informations lues de l'écran
-struct datasRead
-{
-  int id; // Si 0 alors il n'y a pas de données, <0 Numero commande non traitée, >1 Numero de la commande traité
-  char command[80];
-  char name[80];
-  int type;
-  char line[2048];
-};
-
 void menu()
 {
   std::cout << std::endl;
@@ -184,166 +174,13 @@ std::string intToString(int value, std::string formatStr)
   return (buffer);
 };
 
-// Lire le contenu disponible sur le port série
-// Retourne les données dans la structure local datasRead
-// Si dataRead.id == 0 alors il n'y a pas de données lues sur le port
-// Exemple de données qui peuvent être lues sur le port:
-//   Basic format(Voir la documentation de l'écran pour plus de détails):
-//     Frame head  : ST<
-//     Command     : 0x1068 (exemple)
-//     Longeur     : 0x0004 (exemple)
-//     Data        : ctew   (exemple)
-//     Key Value   : 0x01   (exemple)
-//     Frame Tail  : >ET
-//     CRC         : AB 24  (exemple)
-datasRead getValidsDatasIfExists()
-{
-  int n, ii;
-  datasRead rd;
-
-  int commande, longeur;
-  char data[2048];
-  struct
-  {
-    union
-    { // Commande
-      unsigned short hexaShort;
-      struct
-      {
-        char c1;
-        char c2;
-      } c;
-    } shortDataCommand;
-    union
-    { // Longeur
-      unsigned short hexaShort;
-      struct
-      {
-        char c1;
-        char c2;
-      } c;
-    } shortDataLongeur;
-
-  } shortData;
-
-  // Initialisation
-  rd.id = 0; // 0 : Pas de données
-  rd.line[0] = 0x00;
-  rd.command[0] = 0x00;
-  rd.name[0] = 0x00;
-  rd.type = 0;
-
-  // Essai de trouver un S
-  n = stone->readIt((char *)data, 1);
-  while ((n == 1) && (data[0] != 'S'))
-  {
-    n = stone->readIt((char *)data, 1);
-  };
-  if (n <= 0)
-    return (rd);
-  // std::cout << "S FOUND\n";
-
-  // Essai de trouver un T
-  n = stone->readIt((char *)data, 1);
-  while ((n == 1) && (data[0] != 'T'))
-  {
-    n = stone->readIt((char *)data, 1);
-  };
-  if (n <= 0)
-    return (rd);
-  // std::cout << "T FOUND\n";
-
-  // Essai de trouver un <T>
-  n = stone->readIt((char *)data, 1);
-  while ((n == 1) && (data[0] != '<'))
-  {
-    n = stone->readIt((char *)data, 1);
-  };
-  if (n <= 0)
-    return (rd);
-  // std::cout << "< FOUND\n";
-
-  // Lecture de la commande et de la longeur de la donnee
-  n = stone->readIt(&shortData.shortDataCommand.c.c1, 4);
-
-  // Inverser certaines données
-  std::swap(shortData.shortDataCommand.c.c2, shortData.shortDataCommand.c.c1);
-  std::swap(shortData.shortDataLongeur.c.c2, shortData.shortDataLongeur.c.c1);
-
-  commande = shortData.shortDataCommand.hexaShort;
-  longeur = shortData.shortDataLongeur.hexaShort;
-
-  // Lecture Data
-  n = stone->readIt((char *)data, longeur);
-
-  switch (commande)
-  {
-
-  case 0x0002:
-  { // Version
-    int keyValue = (int)data[longeur - 1];
-    data[longeur - 1] = 0x00;
-
-    // Lire les données suivantes : TAIL (3 char ">ET") et CRC (Hexa16)
-    char TailDatas[5];
-    n = stone->readIt(TailDatas, 5);
-    // Check if TAIL (>ET) is OK
-    if ((n != 5) || (TailDatas[0] != '>') || (TailDatas[1] != 'E') || (TailDatas[2] != 'T'))
-      return (rd);
-    // Nous ne vérifions pas le CRC pour plus de rapidité mais ce serait mieux...
-    // Traitement du CRC
-    // int crc = TailDatas[4]; crc <<= 8; crc |= TailDatas[3];
-    // std::cout << "Crc: " << intToString(crc, "%4X") << "\n";
-
-    rd.id = commande;
-    strcpy(rd.command, "Version");
-    strcpy(rd.name, data);
-    rd.type = keyValue;
-
-    return (rd);
-    break;
-  };
-
-  default:
-  {
-
-    int keyValue = (int)data[longeur - 1];
-    data[longeur - 1] = 0x00;
-    // std::cout << "Key Value: " << keyValue << "\n";
-    // std::cout << "Data: " << data << "\n";
-
-    // Lire les données suivantes : TAIL (3 char ">ET") et CRC (Hexa16)
-    char TailDatas[5];
-    n = stone->readIt(TailDatas, 5);
-
-    // Check if TAIL (>ET) is OK
-    if ((n != 5) || (TailDatas[0] != '>') || (TailDatas[1] != 'E') || (TailDatas[2] != 'T'))
-      return (rd);
-    // Nous ne vérifions pas le CRC pour plus de rapidité mais ce serait mieux...
-    // Traitement du CRC
-    // int crc = TailDatas[4]; crc <<= 8; crc |= TailDatas[3];
-    // std::cout << "Crc: " << intToString(crc, "%4X") << "\n";
-
-    rd.id = -commande;
-    strcpy(rd.command, "???????");
-    strcpy(rd.name, data);
-    rd.type = keyValue;
-
-    return (rd);
-    break;
-  };
-  };
-
-  return (rd);
-};
-
 // Thread qui permet de LOOPER et lire le contenu du port serie
 // avec l'aide du la fonction getValidsDatasIfExists
 void fonctionLoop()
 {
   while (true)
   {
-    datasRead rd = getValidsDatasIfExists();
+    dataRead rd = stone->getValidsDatasIfExists();
     //std::cout << "GData : " << intToHexa(abs(rd.id)) << " " << rd.command << " " << rd.name << " " << rd.type << "\n";
     switch (rd.id)
     {
--- Stone/InterfaceEnfichable/stone.cpp
+++ Stone/InterfaceEnfichable/stone.cpp
@@ -3,6 +3,159 @@
 
 Stone::Stone(std::string portName):MySerial{portName}{};
 
+// Lire le contenu disponible sur le port série
+// Retourne les données dans la structure local dataRead
+// Si dataRead.id == 0 alors il n'y a pas de données lues sur le port
+// Exemple de données qui peuvent être lues sur le port:
+//   Basic format(Voir la documentation de l'écran pour plus de détails):
+//     Frame head  : ST<
+//     Command     : 0x1068 (exemple)
+//     Longeur     : 0x0004 (exemple)
+//     Data        : ctew   (exemple)
+//     Key Value   : 0x01   (exemple)
+//     Frame Tail  : >ET
+//     CRC         : AB 24  (exemple)
+dataRead Stone::getValidsDatasIfExists()
+{
+  int n, ii;
+  dataRead rd;
+
+  int commande, longeur;
+  char data[2048];
+  struct
+  {
+    union
+    { // Commande
+      unsigned short hexaShort;
+      struct
+      {
+        char c1;
+        char c2;
+      } c;
+    } shortDataCommand;
+    union
+    { // Longeur
+      unsigned short hexaShort;
+      struct
+      {
+        char c1;
+        char c2;
+      } c;
+    } shortDataLongeur;
+
+  } shortData;
+
+  // Initialisation
+  rd.id = 0; // 0 : Pas de données
+  rd.line[0] = 0x00;
+  rd.command[0] = 0x00;
+  rd.name[0] = 0x00;
+  rd.type = 0;
+
+  // Essai de trouver un S
+  n = readIt((char *)data, 1);
+  while ((n == 1) && (data[0] != 'S'))
+  {
+    n = readIt((char *)data, 1);
+  };
+  if (n <= 0)
+    return (rd);
+  // std::cout << "S FOUND\n";
+
+  // Essai de trouver un T
+  n = readIt((char *)data, 1);
+  while ((n == 1) && (data[0] != 'T'))
+  {
+    n = readIt((char *)data, 1);
+  };
+  if (n <= 0)
+    return (rd);
+  // std::cout << "T FOUND\n";
+
+  // Essai de trouver un <T>
+  n = readIt((char *)data, 1);
+  while ((n == 1) && (data[0] != '<'))
+  {
+    n = readIt((char *)data, 1);
+  };
+  if (n <= 0)
+    return (rd);
+  // std::cout << "< FOUND\n";
+
+  // Lecture de la commande et de la longeur de la donnee
+  n = readIt(&shortData.shortDataCommand.c.c1, 4);
+
+  // Inverser certaines données
+  std::swap(shortData.shortDataCommand.c.c2, shortData.shortDataCommand.c.c1);
+  std::swap(shortData.shortDataLongeur.c.c2, shortData.shortDataLongeur.c.c1);
+
+  commande = shortData.shortDataCommand.hexaShort;
+  longeur = shortData.shortDataLongeur.hexaShort;
+
+  // Lecture Data
+  n = readIt((char *)data, longeur);
+
+  switch (commande)
+  {
+
+  case 0x0002:
+  { // Version
+    int keyValue = (int)data[longeur - 1];
+    data[longeur - 1] = 0x00;
+
+    // Lire les données suivantes : TAIL (3 char ">ET") et CRC (Hexa16)
+    char TailDatas[5];
+    n = readIt(TailDatas, 5);
+    // Check if TAIL (>ET) is OK
+    if ((n != 5) || (TailDatas[0] != '>') || (TailDatas[1] != 'E') || (TailDatas[2] != 'T'))
+      return (rd);
+    // Nous ne vérifions pas le CRC pour plus de rapidité mais ce serait mieux...
+    // Traitement du CRC
+    // int crc = TailDatas[4]; crc <<= 8; crc |= TailDatas[3];
+    // std::cout << "Crc: " << intToString(crc, "%4X") << "\n";
+
+    rd.id = commande;
+    strcpy(rd.command, "Version");
+    strcpy(rd.name, data);
+    rd.type = keyValue;
+
+    return (rd);
+    break;
+  };
+
+  default:
+  {
+
+    int keyValue = (int)data[longeur - 1];
+    data[longeur - 1] = 0x00;
+    // std::cout << "Key Value: " << keyValue << "\n";
+    // std::cout << "Data: " << data << "\n";
+
+    // Lire les données suivantes : TAIL (3 char ">ET") et CRC (Hexa16)
+    char TailDatas[5];
+    n = readIt(TailDatas, 5);
+
+    // Check if TAIL (>ET) is OK
+    if ((n != 5) || (TailDatas[0] != '>') || (TailDatas[1] != 'E') || (TailDatas[2] != 'T'))
+      return (rd);
+    // Nous ne vérifions pas le CRC pour plus de rapidité mais ce serait mieux...
+    // Traitement du CRC
+    // int crc = TailDatas[4]; crc <<= 8; crc |= TailDatas[3];
+    // std::cout << "Crc: " << intToString(crc, "%4X") << "\n";
+
+    rd.id = -commande;
+    strcpy(rd.command, "???????");
+    strcpy(rd.name, data);
+    rd.type = keyValue;
+
+    return (rd);
+    break;
+  };
+  };
+
+  return (rd);
+};
+
 void Stone::setTexte(const char* label, const char* texte){
     std::stringstream ss;
     char cmd[99];
@@ -18,7 +171,6 @@ void Stone::setButton(const char* label, const char* texte){
     ss << "ST<{\"cmd_code\":\"set_text\",\"type\":\"button\",\"widget\":\"" << label << "\",\"text\":\"" << texte << "\"}>ET";
     std::string commande = ss.str();
     strcpy(cmd, commande.c_str());
-    std::cout << cmd << " \n";
     writeIt((char*) cmd);
 }
 
